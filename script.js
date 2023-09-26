261c4b8f09dce917a2f06f6c27d6bc97d7ac5641
Nurjaman <nuerjaman27@gmail.com>
2023-09-26 10:15:00 +0700

    memperbaiki bug

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 let input = document.getElementById('inputBox');
 let button = document.querySelectorAll('button');
-
+let simbol = ["%", "*", "/", "-", "+", "="];
 let angka = "";
+
+const calculate = (btnValue) => {
+    if(btnValue === "=" && btnValue !== "") {
+        angka = eval(angka.replace("%", "/100"));
+    }
+    else if(btnValue === 'AC') {
+        angka = "";
+    }
+    else if(btnValue == 'DEL'){
+        angka = angka.toString().slice(0, -1);
+    }
+    else {
+        if (angka === "" && simbol.includes(btnValue)) return;
+        angka += btnValue;
+    }
+    input.value = angka;
+}
+
+button.forEach((button) => {
+    button.addEventListener("click", (e) =>calculate(e.target.dataset.value));
+});
+
+/*
 let arr = Array.from(button);
 arr.forEach(button => {
-    button.addEventListener('click', (e) => {
+    button.addEventListener('click', (e) => {c
         if(e.target.innerHTML == '='){
             angka = eval(angka);
             input.value = angka;
@@ -21,6 +44,6 @@ arr.forEach(button => {
             angka += e.target.innerHTML;
             input.value = angka;
         }
-
     })
 })
+*/
